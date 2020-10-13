1002189112c52dc8f2ca51d5f4fcb7bd1db0bf48
borielvi <borielvi@isu.edu>
2020-10-13 15:51:06 -0700

    Updates

---
+++ precache-manifest.e87f41cfc159c29a1c70b1800fb9a7d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d96dceb3ab406a619160cb7b61b35b",
+    "url": "/react-porfolio/index.html"
+  },
+  {
+    "revision": "6b8f1bdd3a36d2348df5",
+    "url": "/react-porfolio/static/css/main.8ada966a.chunk.css"
+  },
+  {
+    "revision": "a9b9944810f954985542",
+    "url": "/react-porfolio/static/js/2.7649da59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-porfolio/static/js/2.7649da59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8f1bdd3a36d2348df5",
+    "url": "/react-porfolio/static/js/main.a7794980.chunk.js"
+  },
+  {
+    "revision": "aff21b7ac6c208ccbd1e",
+    "url": "/react-porfolio/static/js/runtime-main.b9de304f.js"
+  },
+  {
+    "revision": "74c19ac41c6f7fc1d3738540342a37d5",
+    "url": "/react-porfolio/static/media/Portfolio-Headshot.74c19ac4.png"
+  },
+  {
+    "revision": "4ec4cf4e776ade658b6e3a4076e85ea6",
+    "url": "/react-porfolio/static/media/RVroadtrip.4ec4cf4e.png"
+  },
+  {
+    "revision": "0e677892c2624bd24bf27d80dce20d52",
+    "url": "/react-porfolio/static/media/RunBuddy.0e677892.png"
+  },
+  {
+    "revision": "1a25f68d86b3b1223609fd804fa5b58b",
+    "url": "/react-porfolio/static/media/beetlescope.1a25f68d.png"
+  },
+  {
+    "revision": "0dcc8412361836d7e2a266c27b8737b2",
+    "url": "/react-porfolio/static/media/resume.0dcc8412.htm"
+  }
+]);
