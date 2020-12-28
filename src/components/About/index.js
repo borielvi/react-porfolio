d5db0ebaabf27908a10d86e4ec9b4e4810f03735
borielvi <borielvi@isu.edu>
2020-12-28 12:46:10 -0700

    added about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ const About = () => {
                 <div>
                     <h2>About-Me</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aspernatur suscipit vitae, soluta perferendis ratione. Praesentium iste ut exercitationem molestias asperiores ea tenetur sed recusandae, non facilis maxime distinctio quos?
+                        My name is Elvin Boring. I am a Web designer and Entreprenuer. Business in all it's facets is my passion. I am very analytical so it helps me to look at every little detail in ever portion of the website design. I spent 6 years in the transportation industry working as a Conductor for the two largest railroad companies in the United States which are Union Pacific Railroad and BNSF Railway. I also spent 6 month's going to the University of Arizona's full-stack coding bootcamp. 
                     </p>
                     <p>
                         You can find most of my projects on my<a href="https://github.com/borielvi" target="_blank">github.</a>
